5184b0082753f9170d754b3340a615e615b17ac0
PrashantSNimbalkar <107276876+PrashantSNimbalkar@users.noreply.github.com>
2022-07-27 00:17:56 +0530

    User services Data

---
+++ AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UsersSerrvice.Entities;
+
+
+namespace UsersService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-8FSFFAE\SQLEXPRESS;Database=SHAURYA;Integrated Security=True;");
+        }
+    }
+}
