ee1d8945ac75e01ef3ab5d1788575b317f61caea
PrashantSNimbalkar <107276876+PrashantSNimbalkar@users.noreply.github.com>
2022-07-27 00:24:35 +0530

    Web application 1 Controller

--- WeatherForecastController.cs
+++ WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UsersSerrvice.Controllers
+namespace WebApplication1.Controllers
 {
     [ApiController]
     [Route("[controller]")]
